638c0a66b870f4addce6a0c4cdcc19824e48f6a2
night-crawler <lilo.panic@gmail.com>
2017-05-20 19:48:03 +0300

    wip

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker build -t docker.force.fm/ncrawler/msa_stemmers:latest \
+             -t ncrawler/msa_stemmers:latest \
+             -t ncrawler/msa_stemmers:0.0.2 \
+             .
