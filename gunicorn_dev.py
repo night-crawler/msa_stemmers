5a7a76e7bd8f3c43f7b3574f38ea5d7fbc640710
kornov-rooman <kornov.rooman@gmail.com>
2017-05-22 13:46:34 +0300

    wip

---
+++ gunicorn_dev.py
@@ -0,0 +1,12 @@
+import os
+
+bind = '0.0.0.0:17920'
+workers = os.environ.get('GUNICORN_WORKERS', 4)
+reload = True
+preload_app = True
+raw_env = [
+    'LANG=ru_RU.UTF-8',
+    'LC_ALL=ru_RU.UTF-8',
+    'LC_LANG=ru_RU.UTF-8'
+]
+timeout = os.environ.get('GUNICORN_TIMEOUT', 10)
