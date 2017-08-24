01df90c06d6e2f7c62e9d8c91fde515ef0dfec67
night-crawler <lilo.panic@gmail.com>
2017-08-25 00:26:13 +0300

    wip

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -t docker.force.fm/ncrawler/msa_stemmers:latest \
-             -t docker.force.fm/ncrawler/msa_stemmers:0.0.10 \
+docker build -t docker.force.fm/msa/msa_stemmers:latest \
+             -t docker.force.fm/msa/msa_stemmers:0.0.10 \
              -t ncrawler/msa_stemmers:latest \
              -t ncrawler/msa_stemmers:0.0.10 \
              .
