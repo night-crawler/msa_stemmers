67435d30b1555e33e2ec876a77511f81ab37da20
night-crawler <lilo.panic@gmail.com>
2017-08-25 00:23:18 +0300

    Bumped version: 0.0.7 → 0.0.8

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker build -t docker.force.fm/ncrawler/msa_stemmers:latest \
-             -t docker.force.fm/ncrawler/msa_stemmers:0.0.7 \
+             -t docker.force.fm/ncrawler/msa_stemmers:0.0.8 \
              -t ncrawler/msa_stemmers:latest \
-             -t ncrawler/msa_stemmers:0.0.7 \
+             -t ncrawler/msa_stemmers:0.0.8 \
              .
