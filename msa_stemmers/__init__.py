849737ce6ee5791e3215281eb122ce58307af69a
night-crawler <lilo.panic@gmail.com>
2018-01-13 09:16:50 +0300

    Bumped version: 0.0.13 → 0.0.14

--- msa_stemmers/__init__.py
+++ msa_stemmers/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_jsonrpc import JSONRPC
 
-__version__ = '0.0.13'
+__version__ = '0.0.14'
 
 # global, but you can make it better =) (for instance: app.jsonrpc etc)
 jsonrpc = JSONRPC(service_url='/api', enable_web_browsable_api=True)
